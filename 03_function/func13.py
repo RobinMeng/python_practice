db77ca37801ed45e1e1ccf765f58fbec481fefe0
robinmeng <robinmeng2015@gmail.com>
2025-11-16 19:47:49 +0800

    更新函数

--- 03_function/func13.py
+++ 03_function/func13.py
@@ -7,4 +7,12 @@ def func(a: int, b: str, c: bool) -> str:
     return '123'
 
 
+## 参数注释
+def func02(a: 'str字符串', b: 'bool值', c: 'number 值') -> '返回字符串':
+    print(a, b, c)
+    return 'hello world'
+
+
 print(func(1, '2', False))
+
+print(func02(1, 2, 3))
