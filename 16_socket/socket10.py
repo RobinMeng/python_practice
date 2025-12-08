eb61e5662ce62b62eca0b819c4c2e4b7b3fbad93
robinmeng <robinmeng2015@gmail.com>
2025-12-08 21:17:11 +0800

    socket_file_tmp

--- 16_socket/socket10.py
+++ 16_socket/socket10.py
@@ -1,1 +1,16 @@
-## 广播
+## 客户端广播
+import socket
+
+ip = "127.0.0.1"
+port = 8000
+
+
+def client():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    pass
+
+
+if __name__ == '__main__':
+    client()
