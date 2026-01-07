548fe897f63aa4e82b59c13f4e1984154f3bc11e
robinmeng <robinmeng2015@gmail.com>
2026-01-07 22:19:07 +0800

    qt

---
+++ 17_qt/qt06/qt06.py
@@ -0,0 +1,19 @@
+import sys
+
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from qtUI import Ui_MainWindow
+
+
+class MyMainWindows(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    windows = MyMainWindows()
+    windows.show()
+    sys.exit(app.exec_())
+    pass
